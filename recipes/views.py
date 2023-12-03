d21908ba820bc84cf0f17b9e064c371b35835745
Luis Carlos Gassner <luisnote@gmail.com>
2023-12-03 19:57:47 -0300

    Criando o card receita1

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html',context={
+    return render(request, 'recipes/pages/home.html',context={
         'name': 'Luis Carlos'
     })
